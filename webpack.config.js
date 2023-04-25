f896d02f998f4aa89240b4d7e621d1f8fffc9280
Verbiturum <user@example.com>
2023-04-26 00:16:19 +0300

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+	mode: "production",
+	entry: "./src/script.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+
+			{
+
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+				type: 'asset/resource',
+
+			},
+		],
+	},
+	optimization: {
+		minimize: false,
+	},
+};
