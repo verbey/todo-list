5a0890cc987cf4b2478ba5dd356b083528b4cf8f
Verbiturum <user@example.com>
2023-07-19 19:21:33 +0300

    Add Workspace module

---
+++ src/modules/workspace.js
@@ -0,0 +1,12 @@
+"use strict";
+
+class Workspace {
+	constructor(title, description, todos, color) {
+		this.title = title;
+		this.description = description;
+		this.todos = todos;
+		this.color = color;
+	}
+}
+
+export default Workspace;
--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
+"use strict";
+
 import Todo from "./modules/todo.js";
+import Workspace from "./modules/workspace.js";
+
 const noscript = document.querySelector(".noscript");
 noscript.remove();
