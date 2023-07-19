f1f8cfab421b171c8c4f6af67609e09c8ed42c0b
Verbiturum <user@example.com>
2023-07-19 20:20:03 +0300

    Add Dom module

--- main.js
+++ main.js
@@ -2,6 +2,25 @@
 /******/ 	"use strict";
 var __webpack_exports__ = {};
 
-const noscript = document.querySelector(".noscript").remove();
+;// CONCATENATED MODULE: ./src/modules/dom.js
+const Dom = (function () {
+	const content = document.querySelector("#content");
+
+	const openWorkspace = () => {
+		// function logic goes here
+	};
+	return { openWorkspace };
+})();
+
+/* harmony default export */ const dom = ((/* unused pure expression or super */ null && (Dom)));
+;// CONCATENATED MODULE: ./src/script.js
+
+
+
+
+
+
+const noscript = document.querySelector(".noscript");
+noscript.remove();
 /******/ })()
 ;
